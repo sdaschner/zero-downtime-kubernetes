6e4db58a2da141bbeee4baacf79909029cb47176
Sebastian Daschner <git@sebastian-daschner.de>
2018-10-18 16:01:14 -0700

    removed superflous script

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,3 @@
 FROM tomee:8-jre-7.0.4-plus
 
-COPY deployment/docker/deactivate-health-check.sh /
-
 COPY target/coffee-shop.war /usr/local/tomee/webapps/
--- src/main/java/com/sebastian_daschner/zero_downtime_k8s/HealthResource.java
+++ src/main/java/com/sebastian_daschner/zero_downtime_k8s/HealthResource.java
@@ -1,29 +1,17 @@
 package com.sebastian_daschner.zero_downtime_k8s;
 
-import javax.ejb.Singleton;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 @Path("health")
 @Produces(MediaType.TEXT_PLAIN)
-@Singleton
 public class HealthResource {
-
-    private final AtomicBoolean shutdown = new AtomicBoolean(false);
 
     @GET
     public String health() {
-        if (shutdown.get())
-            throw new ServiceUnavailableException();
-
         return "OK";
-    }
-
-    // just an example; don't make this publicly accessible :-)
-    @DELETE
-    public void shutdown() {
-        shutdown.set(true);
     }
 
 }
