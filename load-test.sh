b22401ced88c00fd1459387f9b06f3b47fa48a51
Sebastian Daschner <git@sebastian-daschner.de>
2019-03-08 12:11:32 -0500

    updated content

--- load-test.sh
+++ load-test.sh
@@ -7,5 +7,4 @@ HOST=$(kubectl-istio-gateway-ip-address)
 #HOST="192.168.99.100:$PORT"
 #HOST="35.204.171.231"
 
-kubectl exec -it $FORTIO_POD  -c fortio /usr/local/bin/fortio -- load -a -c 8 -qps 500 -t 59s -loglevel Warning http://$HOST/coffee-shop/resources/coffee
-#kubectl exec -it $FORTIO_POD  -c fortio /usr/local/bin/fortio -- load -a -c 2 -qps 0 -n 100 -loglevel Warning http://$HOST/coffee-shop/resources/coffee
+kubectl exec -it $FORTIO_POD  -c fortio /usr/local/bin/fortio -- load -a -c 50 -qps 500 -t 40s -loglevel Warning http://$HOST/coffee-shop/resources/coffee
