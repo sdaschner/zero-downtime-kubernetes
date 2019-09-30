e91305d867d64cae9f00ad0cab6ab4eb6fd8d7c1
Sebastian Daschner <git@sebastian-daschner.de>
2019-09-30 12:24:38 +0300

    added failure for canary

--- src/main/java/com/sebastian_daschner/zero_downtime_k8s/CoffeeResource.java
+++ src/main/java/com/sebastian_daschner/zero_downtime_k8s/CoffeeResource.java
@@ -2,16 +2,17 @@ package com.sebastian_daschner.zero_downtime_k8s;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.WebApplicationException;
+import java.util.Random;
 
 @Path("coffee")
 public class CoffeeResource {
 
     @GET
     public String getCoffee() {
-        return "Coffee\n";
+        if (new Random().nextDouble() < 0.9)
+            throw new WebApplicationException();
+        return "Coffee!\n";
     }
 
 }
