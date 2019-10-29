52266a12a27bec4441d6fdca19ac846e4cf0b1ab
Sebastian Daschner <git@sebastian-daschner.de>
2019-10-29 09:05:53 +0300

    updated coffee response

--- src/main/java/com/sebastian_daschner/zero_downtime_k8s/CoffeeResource.java
+++ src/main/java/com/sebastian_daschner/zero_downtime_k8s/CoffeeResource.java
@@ -2,21 +2,13 @@ package com.sebastian_daschner.zero_downtime_k8s;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.WebApplicationException;
-import java.util.Random;
 
 @Path("coffee")
 public class CoffeeResource {
 
     @GET
     public String getCoffee() {
-        return "Coffee?\n";
-    }
-
-    @GET
-    @Path("fail")
-    public String fail() {
-        throw new WebApplicationException(500);
+        return "Coffee\n";
     }
 
 }
