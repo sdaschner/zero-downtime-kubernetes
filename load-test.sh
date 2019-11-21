58f36223253d94798e22633bba4de6d19e7f961c
Sebastian Daschner <git@sebastian-daschner.de>
2019-11-21 12:31:57 +0300

    changed runtime to Quarkus; updated slides;

--- load-test.sh
+++ load-test.sh
@@ -3,4 +3,4 @@
 FORTIO_POD=$(kubectl get pods | grep fortio | awk '{ print $1 }')
 HOST=$(kubectl get ingress coffee-shop -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
-kubectl exec -it $FORTIO_POD  -c fortio /usr/local/bin/fortio -- load -a -c 50 -qps 500 -t 40s -loglevel Warning http://$HOST/coffee-shop/resources/coffee
+kubectl exec -it $FORTIO_POD  -c fortio /usr/local/bin/fortio -- load -a -c 50 -qps 500 -t 20s -loglevel Warning http://$HOST/coffee
